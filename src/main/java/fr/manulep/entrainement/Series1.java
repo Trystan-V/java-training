38dc50f68aa1f8622a0a66d5184b165d5823cd07
Trystan VALENTIN <trystan.valentin@gmail.com>
2020-10-07 16:14:18 +0200

    Test 4,5,12,13,14,15 Done

--- src/main/java/fr/manulep/entrainement/Series1.java
+++ src/main/java/fr/manulep/entrainement/Series1.java
@@ -1,5 +1,7 @@
 package fr.manulep.entrainement;
 
+import java.util.List;
+
 public class Series1 {
 	
 	private Series1() {
@@ -20,42 +22,50 @@ public class Series1 {
 
 	public static String[] removeNullElements(String[] array) {
 		//2
-		// On veut connaitre la longueur du tableau
-		// on parcourt le tableau et on enlève les null
-		//String[] sortie; //on créer directement un tableau avec la valeur qu'on attend
- /*
-		int cpt=array.length; //la variable qui récupere le nombre de donnée dans le tableau (.lenght)
-		for (int i = 0; i < array.length; i++) {//parcour tout le tableau
-			String valeur= array[i];
-			if (valeur==null) { //Si il ya un null 
-				cpt--; 
-			}
-		}
-	}
-*/		return null;
+	// 	// On veut connaitre la longueur du tableau
+	// 	// on parcourt le tableau et on enlève les null
+	// 	String[] sortie; //on créer directement un tableau avec la valeur qu'on attend
+ 
+	// 	int cpt=array.length; //la variable qui récupere le nombre de donnée dans le tableau (.lenght)
+	// 	for (int i = 0; i < array.length; i++) {//parcour tout le tableau
+	// 		String valeur= array[i];
+	// 		if (valeur==null) { //Si il ya un null 
+	// 			cpt--; 
+	// 		}
+	// 	}
+	// }
+		return null;
 	}
 	
 	public static int[] addElementToBeginning(int[] array, int element) {
 		//3
-
-
-		//int tableau[] = new int[1];
-		//	tableau[0] = 1;
 		return null;
+
 	}
 	
 	public static int[] allElementsExceptFirstThree(int[] array) {
 		//4
-		return null;
+		int[] Sortie = {}; // ce qu'on veut à la fin
+		if (array.length > 3) {
+			Sortie = new int[array.length - 3];
+			for (int i = 3; i < array.length; i++) {
+				Sortie[i - 3]=array[i];
+			} return Sortie;
+		} else {
+			return Sortie;
+		}
 	}
 	
 	public static String getFirstHalf(String word) {
 		//5
-		if (word == "dragon") {
-			return "dra";
-		} else ; {
-			return "sna";
-		}
+	String result; // Résultat final
+	int division= word.length()%2; // découpe le mot en 2
+	if (division == 0) {
+		result = word.substring(0, (word.length() / 2));
+	} else {
+		result = word.substring(0, (word.length() / 2+1));
+	}
+	return result;
 	}
 	
 	public static String[] selectElementsStartingWithA(String[] array) {
@@ -70,8 +80,11 @@ public class Series1 {
 	
 	public static String[] reverseOrderInArray(String[]array) {
 		//8
+		// String[] arrayOut;
+		
+		// int cpt=array.length;
+		// for (int i = 0; i < array.length; i++)
 		return null;
-
 	}
 
 	public static int[] insertElementInTheMiddleOfAnArray(int[] array, int element) {
@@ -91,22 +104,34 @@ public class Series1 {
 	
 	public static long addingTwoNumbers(long number1, long number2) {
 		//12
-		return -1;
+		return number1 + number2;
 	}
 
 	public static long addingThreeNumbers(long number1, long number2, long number3) {
 		//13
-		return -2;
+		return number1 + number2 + number3;
 	}
 
 	public static long addingSeveralNumbers(final Integer... numbers) {
 		//14
-		return -1;
+		long result = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			result += numbers[i];
+		}
+		return result;
 	}
 
 	public static float makeNegative(float number) {
 		//15
-		return 0;
+		float result;
+		
+		if (number > 0) {
+			result = number-(number*2);
+			return result;
+		} else {
+			return number;
+		}
+		
 	}
 
 	public static boolean checkForSpecialCharacters(String string) {
@@ -130,9 +155,12 @@ public class Series1 {
 	}
 
 	public static boolean isPeer(int number) {
+		
 		//20
-		return false;
+		// int reste = number % 2;
+
+		// if(reste == 0) {
+			return true;
+		//}
 	}
-
-
 }
